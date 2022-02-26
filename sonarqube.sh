b6105565a26cfc9e965d130e222e9ce5baaea860
mamanate01~ <mogirenancy@gmail.com>
2022-02-25 18:45:03 -0600

    newfile

---
+++ sonarqube.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+
+#Description: This is a script to download sonarqube
+#Author:
+echo "logging in as vagrant user"
+sleep 2 
+su - vagrant
+sleep 2
+echo "installing java 11"
+sleep 2
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-openjdk -y
+sleep 2
+
+echo "Download SonarQube latest versions "
+cd /opt 
+sudo yum install wget -y
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+sleep 2
+
+
+echo "Etracting packages"
+sleep 2
+sudo yum install -y
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sleep 2
+
+echo "changing what onwership to user and switch to linux binaries"
+sleep 2
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
+./sonar.sh start
+sleep 2
+
+
+sudo firewall-cmd --permanent --add-port=9000/tcp
+
+echo "checking ip address"
+hostname -I | awk '{print$2}'
+
+echo type the above IP address followed by ':9000' as the url
